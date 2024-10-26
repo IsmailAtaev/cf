81d17ae75d8effc168968850b7a2ad5e3b26fd0e
IsmailAtaev <ataewisma@gmail.com>
2024-10-26 23:15:23 +0500

    feat: models db

--- server/model/models.js
+++ server/model/models.js
@@ -2,8 +2,16 @@ const sequelize = require('../database')
 const { DataTypes } = require('sequelize')
 
 
-const User = sequelize.define('user', {
+
+const Role = sequelize.define("Role", {
     id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
+    name: { type: DataTypes.STRING, unique: true, allowNull: false }
+})
+
+
+const User = sequelize.define('User', {
+    id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
+    roleId: { type: DataTypes.UUID, allowNull: false },
     login: { type: DataTypes.STRING, unique: true },
     password: { type: DataTypes.STRING, unique: true },
     firstname: { type: DataTypes.STRING },
@@ -11,5 +19,37 @@ const User = sequelize.define('user', {
     email: { type: DataTypes.STRING },
     phoneNumber: { type: DataTypes.STRING },
     img: { type: DataTypes.STRING },
-    role: { type: DataTypes.STRING },
 })
+
+
+const Category = sequelize.define("Category", {
+    id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
+    parentId: { type: DataTypes.UUID, allowNull: true },
+    name: { type: DataTypes.STRING, allowNull: false }
+});
+
+const Product = sequelize.define("Product", {
+    id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
+    title: { type: DataTypes.STRING, allowNull: false },
+    description: { type: DataTypes.STRING, allowNull: false },
+    price: { type: DataTypes.FLOAT },
+    categoryId: { type: DataTypes.UUID, allowNull: false },
+    img: { type: DataTypes.ARRAY(DataTypes.STRING) }
+});
+
+
+// Setting up associations
+Category.hasMany(Category, { foreignKey: 'parentId', as: 'subcategories' });
+Category.belongsTo(Category, { foreignKey: 'parentId', as: 'parentCategory' });
+
+
+Category.hasMany(Product, { foreignKey: 'categoryId', as: 'products' });
+Product.belongsTo(Category, { foreignKey: 'categoryId', as: 'category' });
+
+
+module.exports = {
+    Role,
+    User,
+    Category,
+    Product,
+}
